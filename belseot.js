e0817fb7fe7abf3eef84b89a940c4489bc43bcb5
bitwinkel <javier.ruiz.alcaraz@gmail.com>
2018-07-27 11:38:04 +0200

    Update and rename bot.js to belseot.js

--- belseot.js
+++ belseot.js
@@ -83,4 +83,10 @@ bot.on('message', function(user, userID, channelID, message, event) {
 			message: "juegazo!"
 		});
 	}
+	 if (message.toUpperCase().indexOf("MARIC") !=-1) {
+		bot.sendMessage({
+			to: channelID,
+			message: "https://youtu.be/X2j0Sr_6wU4"
+		});
+	}
 });
